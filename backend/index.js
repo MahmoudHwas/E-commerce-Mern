4f62f74e9441d3b80729b2c6ab2fe6fa7022bcc9
MahmoudHwas <mahmoudhwas62@gmail.com>
2025-03-28 20:15:59 +0200

    m

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const router = require("./routes");
-const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser")
 require("dotenv").config();
+const app = express()
 
-const app = express();
-
-// Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-}));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api", router);
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api", router)
+const PORT= 8080 || process.env.PORT;
 
-// Connect to DB (optional: wrap in a condition for serverless)
-connectDB().then(() => {
-    console.log("Database connected successfully");
-}).catch(err => {
-    console.error("Database connection failed:", err);
-});
 
-// Export the app for Vercel
-module.exports = app; // بدل app.listen
+
+connectDB()
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+    
+})
